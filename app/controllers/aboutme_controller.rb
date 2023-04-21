b3fafbe1444cd82e89f1f7effeb9bfd836e18844
Aldo Enriquez <aldoo.enriquez@gmail.com>
2023-04-21 14:08:41 -0600

    feature: last_blog section

--- app/controllers/aboutme_controller.rb
+++ app/controllers/aboutme_controller.rb
@@ -1,5 +1,7 @@
 class AboutmeController < ApplicationController
   def index
     @time = DateTime.current
+    @last_blog =
+      BlogPost.where(state: "published").order("published_at DESC").first
   end
 end
